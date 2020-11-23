432c840a281dfcc27ce003a672a89727245b22dd
KimChol <kimchol2ece@outlook.com>
2020-11-23 21:15:51 +0800

    Tips

---
+++ tips.py
@@ -0,0 +1,18 @@
+# -*- coding = utf-8 -*-
+# @Time : 2020/11/23 21:13
+# @Author : KimChol
+# @File: tips.py
+# @Software : PyCharm
+print("aaa","bbb","ccc")
+print("www","baidu","com",sep=".")
+print("hello",end="")
+print("world",end='\t')
+print("python",end='\n')
+print("end")
+
+'''
+aaa bbb ccc
+www.baidu.com
+helloworld	python
+end
+'''
