7baaccf507dc61ce9602b9513d1444dd4bda2813
KimChol <kimchol2ece@outlook.com>
2020-11-23 23:44:02 +0800

    爬取网页的写法

--- DoubanSpider.py
+++ DoubanSpider.py
@@ -14,23 +14,43 @@ import sqlite3  # 进行SQLite数据库操作
 def main():
     baseurl = "https://movie.douban.com/top250"
     # 爬取网页
-    datalist=getData(baseurl)
-    savepath=".\\豆瓣电影Top250.xls"
+    datalist = getData(baseurl)
+    savepath = ".\\豆瓣电影Top250.xls"
     # 保存数据
-    saveData(savepath)
+    # saveData(savepath)
+    askURL("https://movie.douban.com/top250")
 
 
 # 爬取网页
 def getData(baseurl):
     datalist = []
-    # 逐一解析数据
+    for i in range(0,10): # 调用获取页面信息的函数，10次
+        url = baseurl + str(i*25)
+        html = askURL(url) # 保存获取到的网页源码
+            # 逐一解析数据
     return datalist
 
+
+# 得到指定一个url的网页内容
+def askURL(url):
+    head = { # 模拟浏览器头部信息，向服务器发送消息
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.198 Safari/537.36"
+    }  # 用户代理，表示告诉网站我是什么类型的机器，浏览器（本质上是告诉浏览器我们可以接收什么水平的文件内容）
+    request = urllib.request.Request(url,headers=head)
+    html = ""
+    try:
+        response = urllib.request.urlopen(request)
+        html = response.read().decode("utf-8")
+        print(html)
+    except urllib.URLError as e:
+        if hasattr(e,"code"):
+            print(e.code)
+        if hasattr(e,"reaspn"):
+            print(e.reason)
+    return html
 # 保存数据
 def saveData(savepath):
     print("132")
-
-
 
 
 if __name__ == "__main__":  # 如果执行主函数，且运行函数名称为main时
