cddfece231952f513d717fc40d0c51e1cb42fcee
KimChol <kimchol2ece@outlook.com>
2020-11-23 22:22:30 +0800

    获取一个POST请求

--- test/testUrllib.py
+++ test/testUrllib.py
@@ -7,5 +7,12 @@
 import urllib.request
 
 # 网页源代码
-response = urllib.request.urlopen('http://www.baidu.com')
-print(response.read().decode('utf-8'))
+# response = urllib.request.urlopen('http://www.baidu.com')
+# print(response.read().decode('utf-8'))  # 对获取到的网页源码进行UTF-8编码
+
+# 获取一个POST请求
+import urllib.parse
+
+data = bytes(urllib.parse.urlencode({"hello": "world"}), encoding="utf-8")
+response = urllib.request.urlopen("http://httpbin.org/post",data=data)
+print(response.read().decode("utf-8"))
