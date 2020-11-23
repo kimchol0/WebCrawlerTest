c1e83b8edb191fb97563c9ce70050ed5e7f484fe
KimChol <kimchol2ece@outlook.com>
2020-11-23 22:02:30 +0800

    引入库

---
+++ DoubanSpider.py
@@ -0,0 +1,19 @@
+# -*- coding = utf-8 -*-
+# @Time : 2020/11/23 21:44
+# @Author : KimChol
+# @File: DoubanSpider.py
+# @Software : PyCharm
+
+import bs4                              #网页解析
+import re                               #正则表达式，进行文字匹配
+import urllib.request,urllib.error      #指定url，获取网页数据
+import xlwt                             #进行excel操作
+import sqlite3                          #进行SQLite数据库操作
+
+def main():
+    print('hello')
+
+
+if __name__ == "__main__": #如果执行主函数，且运行函数名称为main时
+    #调用函数
+    main()
