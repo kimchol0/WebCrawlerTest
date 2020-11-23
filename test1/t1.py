d52a9b6ff97f3eb47743c644df25bcf4e931201c
KimChol <kimchol2ece@outlook.com>
2020-11-23 21:53:26 +0800

    from ? import ?

---
+++ test1/t1.py
@@ -0,0 +1,9 @@
+# -*- coding = utf-8 -*-
+# @Time : 2020/11/23 21:49
+# @Author : KimChol
+# @File: t1.py
+# @Software : PyCharm
+def add(a,b):
+    return a+b
+
+# print(add(2,3))
---
+++ test2/t2.py
@@ -0,0 +1,9 @@
+# -*- coding = utf-8 -*-
+# @Time : 2020/11/23 21:50
+# @Author : KimChol
+# @File: t2.py
+# @Software : PyCharm
+
+from test1 import t1
+
+print(t1.add(3,5))
